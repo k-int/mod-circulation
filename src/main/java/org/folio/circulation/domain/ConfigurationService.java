921db87479da1dec9d284e4e70211618e81b2666
Marc Johnson <marc@k-int.co.uk>
2019-03-07 15:44:53 +0000

    Scratch commit

--- src/main/java/org/folio/circulation/domain/ConfigurationService.java
+++ src/main/java/org/folio/circulation/domain/ConfigurationService.java
@@ -1,28 +1,39 @@
 package org.folio.circulation.domain;
 
-import io.vertx.core.json.JsonObject;
+import static org.folio.circulation.domain.MultipleRecords.from;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import io.vertx.core.json.JsonObject;
 
 class ConfigurationService {
+  private final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private static final DateTimeZone DEFAULT_DATE_TIME_ZONE = DateTimeZone.UTC;
   private static final String TIMEZONE_KEY = "timezone";
 
   DateTimeZone findDateTimeZone(JsonObject representation) {
-    AdjacentConfigurations configurations = new AdjacentConfigurations(representation);
-    int totalRecords = configurations.getTotalRecords();
-    if (totalRecords <= 0) {
-      return DEFAULT_DATE_TIME_ZONE;
-    }
+    return from(representation, TimeZoneConfig::new, "configs")
+      .map(MultipleRecords::getRecords)
+      .map(this::findDateTimeZone)
+      .orElse(DEFAULT_DATE_TIME_ZONE);
+  }
 
-    List<TimeZoneConfig> timeZoneConfigs = configurations.getTimeZoneConfigs();
-    return timeZoneConfigs.stream()
+  DateTimeZone findDateTimeZone(Collection<TimeZoneConfig> timeZoneConfigs) {
+    final DateTimeZone chosenTimeZone = timeZoneConfigs.stream()
       .map(this::applyTimeZone)
       .findFirst()
       .orElse(DEFAULT_DATE_TIME_ZONE);
+
+    log.info("Chosen timezone: `{}`", chosenTimeZone);
+
+    return chosenTimeZone;
   }
 
   private DateTimeZone applyTimeZone(TimeZoneConfig item) {
